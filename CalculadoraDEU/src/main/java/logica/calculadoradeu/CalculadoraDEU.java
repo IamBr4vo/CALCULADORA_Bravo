e27d53d38470e860ca17139e45a0ea24510453dc
IamBr4vo <bravo9386@gmail.com>
2023-10-04 10:33:48 -0600

    Implementacion de la calculadora

---
+++ CalculadoraDEU/src/main/java/logica/calculadoradeu/CalculadoraDEU.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica.calculadoradeu;
+
+import GUI.Calculadora;
+
+/**
+ *
+ * @author Bravo
+ */
+public class CalculadoraDEU {
+
+    public static void main(String[] args) {
+        Calculadora c = new Calculadora();
+        c.setVisible(true);
+        c.setResizable(false);
+        c.setLocationRelativeTo(null);
+        
+    }
+}
